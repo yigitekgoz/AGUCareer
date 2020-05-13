4fe6d4a80b9c3289fef6a8c5053a6a7b6a75adbd
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-13 17:51:30 +0300

    Missing Files
    Missing files are included, hopefully.

--- agu_career_v1_0/lib/services/auth_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class AuthService {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-  Future sendPasswordResetEmail(String email) async {
-    return _firebaseAuth.sendPasswordResetEmail(email: email);
-  }
-}
---
+++ agucareer/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:agucareer/hosgeldin_widget/hosgeldin_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HosgeldinWidget(),
+    );
+  }
+}
