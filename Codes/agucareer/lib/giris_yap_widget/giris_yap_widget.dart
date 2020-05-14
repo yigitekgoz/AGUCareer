756724152c48d55a30df94cc4d27bbf2b7a77328
Raşit Aydın <aydin.rasit.m@gmail.com>
2020-05-14 18:00:02 +0300

    nothing

--- Codes/agucareer/lib/giris_yap_widget/giris_yap_widget.dart
+++ Codes/agucareer/lib/giris_yap_widget/giris_yap_widget.dart
@@ -1,7 +1,7 @@
-import 'package:agucareerv10/profil_duzenle_widget/profil_duzenle_widget.dart';
-import 'package:agucareerv10/profil_widget/profil_widget.dart';
-import 'package:agucareerv10/sifremi_unuttum_widget/sifremi_unuttum_widget.dart';
-import 'package:agucareerv10/values/colors.dart';
+import 'package:agucareer/profil_duzenle_widget/profil_duzenle_widget.dart';
+import 'package:agucareer/profil_widget/profil_widget.dart';
+import 'package:agucareer/sifremi_unuttum_widget/sifremi_unuttum_widget.dart';
+import 'package:agucareer/values/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
